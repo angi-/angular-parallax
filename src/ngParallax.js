ddd6d6dcc1ff05355ce41b8d4c39510742679008
Angelin <angelin.sirbu@vola.ro>
2015-08-07 16:38:33 +0300

    Added more documentation

--- src/ngParallax.js
+++ src/ngParallax.js
@@ -49,34 +49,56 @@ angular
            */
           cssValArray;
 
+        // Find out if parallax affects an element top position or CSS property
         parallaxCssVal = attr.parallaxCss ? attr.parallaxCss : 'top';
+
+        // Set the CSS key and value
         cssValArray = parallaxCssVal.split(':');
         cssKey = cssValArray[0];
         cssValue = cssValArray[1];
 
         isSpecialVal = cssValue ? true : false;
-        if (!cssValue) cssValue = cssKey;
+        if ( ! cssValue) cssValue = cssKey;
 
         parallaxRatio = attr.parallaxRatio ? +attr.parallaxRatio : 1.1;
         parallaxInitVal = attr.parallaxInitVal ? +attr.parallaxInitVal : 0;
 
         elm.css(cssKey, parallaxInitVal + 'px');
 
+        /**
+         * Function called by the scroll and touch move events
+         */
         function _onScroll()
         {
           var
+            /**
+             * Result value applied to element's style
+             * @type string
+             */
             resultVal,
+
+            /**
+             * Calculated value
+             * @type number
+             */
             calcVal = $window.pageYOffset * parallaxRatio + parallaxInitVal;
 
-          if (isSpecialVal) {
+          if (isSpecialVal)
+          {
+            // Special values
             resultVal = '' + cssValue + '(' + calcVal + 'px)';
-          } else {
+          }
+          else
+          {
+            // Normal values
             resultVal = calcVal + 'px';
           }
 
+          // Apply CSS
           elm.css(cssKey, resultVal);
         };
 
+        // Listen for scroll and touch move events
         $window.addEventListener('scroll', _onScroll);
         $window.addEventListener('touchmove', _onScroll);
       }
